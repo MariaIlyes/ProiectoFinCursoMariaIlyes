1f819d6d87cde06db5f470133cddae146d4fd255
Maria Ilyes <mariaylyes76@gmail.com>
2024-04-20 21:15:26 +0200

    subiendo las carpetas models y controllers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser  from'body-parser';
 import {conexion} from'./config/database.js';
 const app = express();
 
-const port = 3000;
+const port = 5000;
 
 app.use(express.static('views'));
 app.set('view engine' , 'ejs');
---
+++ models/modelEmpleado.js
@@ -0,0 +1,14 @@
+class Empleado{
+    constructor(id, nombre, apellidos, telefono, nif, email, contrasena, usuario){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.nif = nif;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.usuario = usuario;
+    }
+}
+
+export default Empleado;
