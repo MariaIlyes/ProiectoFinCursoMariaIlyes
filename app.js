d197ef3da54291552b434331cbad6a4dc56ccf92
Maria Ilyes <mariaylyes76@gmail.com>
2024-04-27 21:29:28 +0200

    He modificado de manera que soy capaz de realizar el hash a la contraseña, guardarla en la bd, y logarme, y tambien comprobar si el usuario existe o no, su contraseña esta ok o no

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {conexion} from './config/database.js';
+import { conexion } from './config/database.js';
+import bcrypt from 'bcryptjs';
 
 const app = express();
 
@@ -8,7 +9,10 @@ const port = 5000;
 
 app.use(express.static('views'));
 
-app.use(bodyParser.urlencoded({ extended : true}));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/crearUsuario', (req, res) => {
@@ -20,7 +24,7 @@ app.post('/crearUsuario', (req, res) => {
     const email = req.body.email;
     const contrasena = req.body.contrasena;
     const usuario = req.body.usuario;
-    conexion.query('USE maitdb;',()=>{});
+    conexion.query('USE maitdb;', (err, result) => { });
     // Verificar si el usuario ya existe en la tabla
     conexion.query(`SELECT COUNT(*) AS count FROM usuarios WHERE usuario = '${usuario}'`, (err, result) => {
         if (err) {
@@ -28,68 +32,106 @@ app.post('/crearUsuario', (req, res) => {
             res.status(500).send('Error interno del servidor');
             return;
         }
-        console.log(result.length );
+        console.log(result.length);
         console.log(result[0].count);
         if (result.length > 0 && result[0].count > 0) {
-            res.render('usuarioKO', {data: usuario});
+            // http 400
+            res.render('usuarioKO', { data: usuario });
         } else {
             // Insertar nuevo usuario en la tabla
-            const sql = `INSERT INTO usuarios(nombre, apellidos, telefono, nif, email, contrasena, usuario) 
+            bcrypt.hash(contrasena, 10)
+                .then(hash => {
+                    console.log('Hash resultado de aplicar bcryptjs a tu contraseña:', hash);
+                    const sql = `INSERT INTO usuarios(nombre, apellidos, telefono, nif, email, contrasena, usuario) 
                     VALUES (?, ?, ?, ?, ?, ?, ?)`;
-            conexion.query(sql, [nombre, apellidos, telefono, nif, email, contrasena, usuario], (err, result) => {
-                if (err) {
-                    console.error('Error al insertar el usuario:', err);
-                    res.status(500).send('Error interno del servidor');
-                    return;
-                }
-              
-                res.render('usuarioOK', {data: usuario});
-            });
+                    conexion.query(sql, [nombre, apellidos, telefono, nif, email, hash, usuario], (err, result) => {
+                        if (err) {
+                            console.error('Error al insertar el usuario:', err);
+                            res.status(500).send('Error interno del servidor');
+                            return;
+                        }
+                        //http 200
+                        res.render('usuarioOK', { data: usuario });
+                    });
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+
         }
     });
 });
 
 
-app.get('/compania', ( req , res )=>{
+app.get('/compania', (req, res) => {
     res.render('compania');
 });
 
-app.get('/crear', ( req , res )=>{
+app.get('/crear', (req, res) => {
     res.render('crear');
 });
 
-app.get('/eliminar', ( req , res )=>{
+app.get('/eliminar', (req, res) => {
     res.render('eliminar');
 });
 
-app.get('/', ( req , res )=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/modificar', ( req , res )=>{
+app.get('/modificar', (req, res) => {
     res.render('modificar');
 });
 
-app.get('/servicios', ( req , res )=>{
+app.get('/servicios', (req, res) => {
     res.render('servicios');
 });
 
-app.get('/usuarioLogado', ( req , res )=>{
-    res.render('usuarioLogado',{data: req.query.usuario});
+app.get('/usuarioLogado', (req, res) => {
+    console.log('Usuario:', req.query.usuario);
+    console.log('Contrasena', req.query.contrasena);
+    const usuario = req.query.usuario;
+    const password = req.query.contrasena;
+    // reicbo el req y de allí recogo el usuario y su contrasena // luego hago un select a la DB para ver si el usuario existe:
+    // si no existe doy mensaje "usuario" no existe   // si existe cojo su contrasena hasheada guardada en la db y la comparo con la contrasena hasheada del req.body
+    conexion.query('USE maitdb;', (err, result) => { });
+    conexion.query(`SELECT contrasena FROM usuarios WHERE usuario = '${usuario}'`, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            if (result.length > 0) {
+                console.log('Hashul salvat in baza de date este:', result[0].contrasena);
+                bcrypt.compare(password, result[0].contrasena)
+                    .then(match => {
+                        if (match) {
+                            console.log('Contrasena OK (correcta)!');
+                            res.render('usuarioLogado', { data: req.query.usuario });
+                        } else {
+                            console.log('Contrasena KO (incorrecta)!');
+                            // fi atent ca renderul urmator e de proba doar
+                            res.render('usuarioKO', { data: req.query.usuario });
+                        }
+                    })
+                    .catch(err => console.error(err));
+            } else {
+                console.log('No existe este usuario!');
+                // fi atent ca renderul urmator e de proba doar
+                res.render('usuarioKO', { data: req.query.usuario });
+            }
+        }
+    });
+
 });
 
-app.get('/views/usuarioNuevo', ( req , res )=>{
+app.get('/views/usuarioNuevo', (req, res) => {
     res.render('usuarioNuevo');
 });
 
-app.get('/views/index', ( req , res )=>{
+app.get('/views/index', (req, res) => {
     res.render('index');
 });
 
-app.set('view engine' , 'ejs');
-app.set('views' , './views');
-
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`El server de tu app está levantado y escuchando en el ${port}`);
     console.log(`Accede http://localhost:${port}`);
 });
