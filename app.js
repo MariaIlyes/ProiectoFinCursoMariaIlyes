c856ed57032d200f97cf3d4e62e8c10312bd4206
Maria Ilyes <mariaylyes76@gmail.com>
2024-05-20 18:25:27 +0200

    tercera entrega final

--- app.js
+++ app.js
@@ -5,49 +5,62 @@ import bcrypt from 'bcryptjs';
 import session from 'express-session';
 import dotenv from 'dotenv';
 
+/**
+ * importamos los modulos necesarios para la aplicación
+ * 
+ */
+
+// cargamos las variables de entorno desde un archivo .env
 dotenv.config();
-
+// creamos una instancia de la aplicación Express
 const app = express();
-
+// establecemos el puerto en el que se ejecutará el servidor
 const port = process.env.PORT;
-
+// establecemos la carpeta views como expuesta para que los ficheros puedan ser accedidos por el cliente
 app.use(express.static('views'));
-
+// definimos el motor de vistas como ejs para poder utilizar javascript embebido
 app.set('view engine', 'ejs');
+// establecemos ./views como carpeta donde el motor ejs buscará las plantillas para renderizar
 app.set('views', './views');
-
+// indicamos a la aplicación que puede parsear datos de tipo urlencoded y json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/**
+ * configuramos el middleware de sesiones para manejar sesiones de usuario
+secret: 'secreto' es la clave que utilizamos para firmar la cookie de sesión,
+y eso asegura que los datos de la sesión estan protegidos (en vez de 'secreto' habría que crear 
+una clave aleatoria y referenciarla desde un fichero de configuración)
+resave: true fuerza la sesión a ser guardada en la tienda de sesiones, incluso si no ha sido modificada
+saveUninitialized: true permite que las sesiones nuevas pero no modificadas se guarden en la tienda de sesiones´*/
 app.use(session({
     secret: 'secreto',
     resave: true,
     saveUninitialized: true
 }));
-
+//definimos ruta que se utilizará para la acción de cerrar session
 app.get('/salir', (req, res) => {
     req.session.destroy((err) => {
         if (err) {
             console.error('Error al cerrar sesión:', err);
-        } else {
+        } else { // si no hy error al destruír la session se renderiza/pinta la pagina index
             res.render('index');
         }
     });
 });
-
+// definimos la ruta para crear un usuario nuevo
 app.post('/crearUsuario', (req, res) => {
     const { nombre, apellidos, telefono, nif, email, contrasena, usuario } = req.body;
-    // Comprobamos si el usuario ya existe en la tabla
+    // comprobamos si el usuario ya existe en la tabla usuarios
     conexion.query(`SELECT COUNT(*) AS count FROM ${process.env.DATABASE}.usuarios WHERE usuario = '${usuario}'`, (err, result) => {
         if (err) {
             res.status(500).send('Error interno del servidor (a la hora de intentar verificar el usuario)');
             return;
         }
-        // si el usuario existe decimos que no permitimos su creación ya que lo duplicaríamos
-        if (result.length > 0 && result[0].count > 0) {
+
+        if (result.length > 0 && result[0].count > 0) {// si el usuario existe decimos que no permitimos su creación ya que lo duplicaríamos
             res.render('usuarioExistente');
-        } else {
-            // si el usuario que se desea crear no existe en la bd, lo insertamos en la tabla
+        } else { // si el usuario que se desea crear no existe en la bd, lo insertamos en la tabla pero hasheando con sal su contrasena con bcryptjs
             bcrypt.hash(contrasena, 10)
                 .then(hash => {
                     const sql = `INSERT INTO ${process.env.DATABASE}.usuarios(nombre, apellidos, telefono, nif, email, contrasena, usuario) 
@@ -57,8 +70,7 @@ app.post('/crearUsuario', (req, res) => {
                             res.status(500).send('Error interno del servidor (al insertar el usuario)');
                             return;
                         }
-                        // e informamos que el usuario se ha creado con éxito
-                        res.render('usuarioOK', { data: usuario });
+                        res.render('usuarioOK', { data: usuario }); // informamos que el usuario se ha creado con éxito
                     });
                 })
                 .catch(err => {
@@ -67,87 +79,81 @@ app.post('/crearUsuario', (req, res) => {
         }
     });
 });
-
+// ruta para ver los componentes de la empresa/compania
 app.get('/compania', (req, res) => {
     res.render('compania');
 });
-
+// ruta para ver el formulario de creación de usuario nuevo
 app.get('/usuarioNuevo', (req, res) => {
     res.render('usuarioNuevo');
 });
-
-app.get('/eliminar', (req, res) => {
+// ruta para ver el formulario de creación de empleado
+app.post('/crear', (req, res) => {
     const user = req.body.user;
-    res.render('eliminar', { user: user });
+    res.render('crear', { user: user });
 });
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/modificar', (req, res) => {
-    const user = req.body.user;
-    res.render('modificar', { user: user });
-});
-
+// ruta para ver los servicios de la empresa
 app.get('/servicios', (req, res) => {
     res.render('servicios');
 });
-
+// ruta de la pagina inicial (donde el usuario no está logado todavia)
+app.get('/', (req, res) => {
+    res.render('index');
+});
+// ruta para renderizar/ver los datos del perfil del usuario
 app.get('/perfil', (req, res) => {
-    if (req.session.user && req.session.user.username) {
-        console.log("User logged session:", req.session.user.username);
+    if (req.session.user && req.session.user.username) { //verificamos que hay session
         conexion.query(`SELECT * FROM ${process.env.DATABASE}.usuarios WHERE usuario = '${req.session.user.username}'`, (err, result) => {
             if (err) {
                 res.status(500).send('Error interno del servidor (a la hora de intentar verificar el usuario)');
                 return;
             }
-            // si el usuario existe decimos que no permitimos su creación ya que lo duplicaríamos
             res.render('perfil', { user: req.session.user.username, data: result });
-
         });
-
-    } else {
-        console.log("No user logged in session");
+    } else { // si no hay session enviamos al principio/home/inicio index
+        res.redirect('/');
     }
 });
-
+// ruta para enviar los datos para crear un empleado nuevo
 app.post('/crearEmpleado', (req, res) => {
-    // no olvides hacer viajar el nombre del usuario de usuarioLogado a crear para que pueda viajar de vuelta
     const { nombre, apellidos, telefono, puesto, sueldo } = req.body;
-    const sql = `INSERT INTO ${process.env.DATABASE}.empleados(nombre, apellidos, telefono, puesto, sueldo, usuario) 
+    const sql = `INSERT INTO ${process.env.DATABASE}.empleados(nombre, apellidos, telefono, puesto, sueldo, idUsuario) 
                     VALUES (?, ?, ?, ?, ?, ?)`;
-    conexion.query(sql, [nombre, apellidos, telefono, puesto, sueldo, req.session.user.username], (err, result) => {
-        if (err) {
-            res.status(500).send('Error interno del servidor (al insertar el empleado)');
-            return;
-        }
-        // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
-        conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
+    if (req.session && req.session.user.id) { // verificamos que hay session
+        conexion.query(sql, [nombre, apellidos, telefono, puesto, sueldo, req.session.user.id], (err, result) => {
             if (err) {
-                res.status(500).send('Error interno del servidor');
+                res.status(500).send('Error interno del servidor (al insertar el empleado)');
                 return;
-            } else {
-                res.render('usuarioLogado', { user: req.session.user.username, data: result });
             }
+            // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
+            conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
+                if (err) {
+                    res.status(500).send('Error interno del servidor');
+                    return;
+                } else {
+                    res.render('usuarioLogado', { user: req.session.user.username, data: result });
+                }
+            });
         });
-    });
+    } else {// si no hay session redirigimos al index
+        res.redirect('/');
+    }
 });
-
+// ruta para pintar la tabla de empleados del usuario logado
 app.post('/usuarioLogado', (req, res) => {
     const usuario = req.body.usuario;
     const password = req.body.contrasena;
-    if (req.session.user && req.session.user.username) { // si ya existe session y username recuperamos los empleados del usuario la bd
-        conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
+    if (req.session.user && req.session.user.username && req.session.user.id) { // si ya existe session recuperamos los empleados del usuario la bd
+        conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
             if (err) {
                 res.status(500).send('Error interno del servidor (al recuperar los empleados)');
                 return;
-            } else { // recuperamos correctamente los empleados del usuario logado
+            } else { // recuperamos correctamente los empleados del usuario logado y los pintamos
                 res.render('usuarioLogado', { user: req.session.user.username, data: result });
             }
         });
-    } else { // si no hay session o no hay username procedemos a recuperar la contrasena del usuario (todavia no logado) de la bd
-        conexion.query(`SELECT contrasena FROM ${process.env.DATABASE}.usuarios WHERE usuario = '${usuario}'`, (err, result) => {
+    } else { // si no hay session o no hay username procedemos a recuperar la contrasena del usuario (todavia no logado pero que ahora se está logando) de la bd
+        conexion.query(`SELECT contrasena, id FROM ${process.env.DATABASE}.usuarios WHERE usuario = '${usuario}'`, (err, result) => {
             if (err) { // error al ejecutar la query de búsqueda de contrasena del usuario en la bd
                 res.status(500).send('Error interno del servidor (al recuperar la contrasena)');
                 return;
@@ -155,9 +161,9 @@ app.post('/usuarioLogado', (req, res) => {
                 if (result.length > 0) {
                     bcrypt.compare(password, result[0].contrasena)
                         .then(match => {
-                            if (match) { // usuario logado; asignamos valor a la req.session.user                                 
-                                req.session.user = { username: usuario }; // guardamos usuario en la session
-                                conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
+                            if (match) { // usuario logado; asignamos los valores usuario (al username) e result[0].id (id) a la req.session.user                                
+                                req.session.user = { username: usuario, id: result[0].id }; // guardamos usuario en la session
+                                conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
                                     if (err) {
                                         res.status(500).send('Error interno del servidor (al recuperar los empleados, habiendo recuperado la contrasena)');
                                         return;
@@ -177,12 +183,7 @@ app.post('/usuarioLogado', (req, res) => {
         });
     }
 });
-
-app.post('/crear', (req, res) => {
-    const user = req.body.user;
-    res.render('crear', { user: user });
-});
-
+// ruta para ver el formulario con los datos del empleado que queremos modificar
 app.post('/modificar', (req, res) => {
     if (req.session.user && req.session.user.username) {
         const { id, nombre, apellidos, telefono, puesto, sueldo } = req.body;
@@ -191,12 +192,16 @@ app.post('/modificar', (req, res) => {
         res.render('index');
     }
 });
-
+// ruta para ver el formulario con los datos del empleado que queremos eliminar
 app.post('/eliminar', (req, res) => {
     const { id, nombre, apellidos, telefono, puesto, sueldo } = req.body;
-    res.render('eliminar', { user: req.session.user.username, id: id, nombre: nombre, apellidos: apellidos, telefono: telefono, puesto: puesto, sueldo: sueldo });
+    if (req.session && req.session.user.username) {
+        res.render('eliminar', { user: req.session.user.username, id: id, nombre: nombre, apellidos: apellidos, telefono: telefono, puesto: puesto, sueldo: sueldo });
+    } else {
+        res.redirect('/');
+    }
 });
-
+// ruta para eliminar empleado existente
 app.post('/eliminarEmpleado', (req, res) => {
     const id = req.body.id;
     conexion.query(`DELETE FROM ${process.env.DATABASE}.empleados WHERE id='${id}'`, (err, result) => {
@@ -204,19 +209,23 @@ app.post('/eliminarEmpleado', (req, res) => {
             res.status(500).send('Error interno del servidor');
             return;
         } else {
-            // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
-            conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
-                if (err) {
-                    res.status(500).send('Error interno del servidor');
-                    return;
-                } else {
-                    res.render('usuarioLogado', { user: req.session.user.username, data: result });
-                }
-            });
+            if (req.session && req.session.user.id && req.session.user.username) {
+                // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
+                conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
+                    if (err) {
+                        res.status(500).send('Error interno del servidor');
+                        return;
+                    } else {
+                        res.render('usuarioLogado', { user: req.session.user.username, data: result });
+                    }
+                });
+            } else {
+                res.redirect('/');
+            }
         }
     });
 });
-
+// ruta para modificar empleado existente
 app.post('/modificarEmpleado', (req, res) => {
     const { id, nombre, apellidos, telefono, puesto, sueldo } = req.body;
     conexion.query(`UPDATE ${process.env.DATABASE}.empleados SET nombre='${nombre}', apellidos= '${apellidos}', telefono='${telefono}',puesto='${puesto}',sueldo='${sueldo}' WHERE id='${id}'`, (err, result) => {
@@ -224,19 +233,23 @@ app.post('/modificarEmpleado', (req, res) => {
             res.status(500).send('Error interno del servidor');
             return;
         } else {
-            // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
-            conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
-                if (err) {
-                    res.status(500).send('Error interno del servidor');
-                    return;
-                } else {
-                    res.render('usuarioLogado', { user: req.session.user.username, data: result });
-                }
-            });
+            if (req.session && req.session.user.username && req.session.user.id) {
+                // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
+                conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
+                    if (err) {
+                        res.status(500).send('Error interno del servidor');
+                        return;
+                    } else {
+                        res.render('usuarioLogado', { user: req.session.user.username, data: result });
+                    }
+                });
+            } else {
+                res.redirect('/');
+            }
         }
     });
 });
-
+// ruta para modificar los datos del usuario
 app.post('/modificarUsuario', (req, res) => {
     const { usuario, nombre, apellidos, telefono, nif, email } = req.body;
     conexion.query(`UPDATE ${process.env.DATABASE}.usuarios SET nombre='${nombre}', apellidos= '${apellidos}', telefono='${telefono}', nif='${nif}',email='${email}' WHERE usuario='${usuario}'`, (err, result) => {
@@ -244,15 +257,19 @@ app.post('/modificarUsuario', (req, res) => {
             res.status(500).send('Error interno del servidor');
             return;
         } else {
-            // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
-            conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE usuario = '${req.session.user.username}'`, (err, result) => {
-                if (err) {
-                    res.status(500).send('Error interno del servidor');
-                    return;
-                } else {
-                    res.render('usuarioLogado', { user: req.session.user.username, data: result });
-                }
-            });
+            if (req.session && req.session.user.username && req.session.user.id) {
+                // recuperamos los datos de la tabla empleados para renderizarlos en usuarioLogado
+                conexion.query(`SELECT * FROM ${process.env.DATABASE}.empleados WHERE idUsuario = '${req.session.user.id}'`, (err, result) => {
+                    if (err) {
+                        res.status(500).send('Error interno del servidor');
+                        return;
+                    } else {
+                        res.render('usuarioLogado', { user: req.session.user.username, data: result });
+                    }
+                });
+            } else {
+                res.redirect('/');
+            }
         }
     });
 });
